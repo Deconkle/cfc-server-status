cd9d0cd9fa4f0a39b163fdfacdabbc5bcc5f4f4e
Deconkle <Chaselikescandy@gmail.com>
2022-04-04 11:02:54 -0500

    Update cfc.py

--- cfc.py
+++ cfc.py
@@ -1,12 +1,15 @@
-import requests
-import json
-from steam import game_servers as gs
+import requests # request API 
+import json # read from API
+from steam import game_servers as gs # so we can make A2S requests
 
 url = 'https://nanny.cfcservers.org/cfc3-status'
-"""
-# There are 12 players connected
-# -------------
-# Name: POLY | Time Connnected: 1:24:01 | [SPAWNING]
+""" example:
+There is currently 12 players connected.
+POLY (STEAM_0:1:99999999)
+    Score: 999
+    in PVP: yes
+    Time Connected: 17:01:32
+etc
 """
 def get_a2s_info(server): 
     try: # this can fail, completely up to the server
@@ -16,14 +19,15 @@ def get_a2s_info(server):
             scores[ply['name']] = ply['score']
         return scores
     except:
-        print("Server A2S error, i'm sure it's fine")
-        pass
+        print("Server A2S error, i'm sure it's fine") 
+        pass # We can pass here, if this doesn't return, Score is simply excluded from the formatted list.
          
 
 def get_player_info():
     r = requests.get(url)
     if r.ok:
         return json.loads(r.text)
+    #error handling is for loosers. jk tho if this is down we're screwed
 
 def format_player_info(info):
     players = [v for v in info['status']['players']]
@@ -31,6 +35,7 @@ def format_player_info(info):
     server = (info['status']['ip'],int(info['status']['port'])) # incase cfc changes ip.. i guess? (instead of hardcoded ip)
     a2s = get_a2s_info(server) or None
     
+    print("There is currently "+str(len(players))+" players connected.")
     for ply in players:
         connecting = ""
         score = ""
@@ -40,11 +45,13 @@ def format_player_info(info):
 
         if ply['state'] == "spawning":
             connecting = "\n\t[SPAWNING]"
+
         else: # If they're spawning, their pvp status doesn't exist and nor does their name in the a2s record
             if pvpinfo[steamid]:
                 in_pvp = "\n\tin PVP: Yes"
             if a2s:
                 score = "\n\tScore: "+str(a2s[ply['name']])
+
         print(name+score+in_pvp+"\n\tTime Connected: "+ply['time_connected']+connecting+"\n")
 
 format_player_info(get_player_info())
