dbb22807c9a1be1f0f7024ffa53e9ef471470eeb
Decline <Chaselikescandy@gmail.com>
2023-03-05 07:15:45 -0600

    Quick formatting update
    hastily changed the printing format to something more compact and visually appealing. I hacked code together as quickly as possible to accomplish this.

--- cfc.py
+++ cfc.py
@@ -3,13 +3,7 @@ import json # read from API
 from steam import game_servers as gs # so we can make A2S requests
 
 url = 'https://nanny.cfcservers.org/cfc3-status'
-""" example:
-POLY (STEAM_0:1:99999999)
-    Score: 999
-    in PVP: yes
-    Time Connected: 17:01:32
-etc
-"""
+
 def get_a2s_info(server): 
     try: # this can fail, completely up to the server
         players = gs.a2s_players(server)
@@ -39,18 +33,19 @@ def format_player_info(info):
         connecting = ""
         score = ""
         steamid = ply['steam_id']
-        name = ply['name']+" ("+steamid+") "
+        name = ply['name']
         in_pvp = ""
 
         if ply['state'] == "spawning":
-            connecting = "\n\t[SPAWNING]"
+            connecting = " [SPAWNING] "
 
         else: # If they're spawning, their pvp status doesn't exist and nor does their name in the a2s record
             if pvpinfo[steamid]:
-                in_pvp = "\n\tin PVP: Yes"
+                in_pvp = " | in PVP"
             if a2s:
-                score = "\n\tScore: "+str(a2s[ply['name']])
+                sid = "("+steamid+")"
+                score = " | Score: "+str(a2s[ply['name']])
 
-        print(name+score+in_pvp+"\n\tTime Connected: "+ply['time_connected']+connecting+"\n")
+        print(name+score+in_pvp+" | "+ply['time_connected']+connecting+"\n"+sid+"\n")
 
 format_player_info(get_player_info())
